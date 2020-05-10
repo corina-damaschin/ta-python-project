c7fcc020bfcc55cd7dcd7027b4dc08a70377e757
Corina Damaschin <corina.damaschin@gmail.com>
2020-05-10 22:55:27 +0300

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 print('You are in Coconuts Branch!')
+#int-float
+a = 2
+b = 54
+c = 13.5
+print(a+b+c)
+
+x = 13
+z = 23.5
+y = x + z
+print (y % x)
+
+a = 2.5
+p = 1
+q = a + 3
+print(q ** 13 //a)
+
+#string
+a = "Hello"
+b = "Coconut"
+print(a + b)
+
+reddish = 1
+lettuce = 3
+apple = 2.5
+sauce = 0.25
+salad = reddish + lettuce + apple + sauce // 4
+print(salad+1.5)
+
+# test = int(3) + 1
+test = int('3') + z
+print("Test: ", test)
+
+text = "В реальной жизни мы совершаем различные действия над окружающими нас предметами"
+print(len(text))
+print(text[:])
+print(text[34])
+print(text[3] + text[45] + text[62] + text[-1])
+print(text[::-1])
+
+text = "В реальной жизни мы совершаем различные действия над окружающими нас предметами"
+print(text.title())
+
+
+
+
